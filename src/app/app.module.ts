77beecc76847dda639d1f00c891fa9b906fb6461
VictorD <dizzy94@gmail.com>
2020-08-26 19:21:25 +0200

    Data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabeceraComponent } from './Components/cabecera/cabecera.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    ServerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  /*SINTAXIS Pe DECLARAR VARIABLES
+   *forma 1
+     -{nombre}: {tipo} = {valor};
+   *forma 2
+     -{nombre}: = {valor};
+
+ */
+
+  nuevoServer: string = "block";
+  serverId: number = 10;
+  serverStatus: string = 'FUNCIONANDO';
+
+
+  ngOnInit(): void {
+    /*llamada a un metodo
+      embeber un metodo
+      *forma1
+      ()=> {lógica en una sola línea}   
+    
+    */
+    setTimeout(() => this.nuevoServer="none",6000);
+  }
+
+
+}
